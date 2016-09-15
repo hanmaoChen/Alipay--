20d2ffe232097c8a0a2d9116dfdba24d2a127553
hanmaoChen <771536653@qq.com>
2016-09-15 22:42:08 +0800

    设置导航栏和状态栏

--- Alipay/Classes/Main/ZFBNavigationController.m
+++ Alipay/Classes/Main/ZFBNavigationController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ZFBNavigationController.h"
-
+#import "HMAdditions.h"
 @interface ZFBNavigationController ()
 
 @end
@@ -16,12 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self setupUI];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//让状态栏变亮
+- (UIStatusBarStyle)preferredStatusBarStyle{
+    
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - 设置导航栏信息
+- (void)setupUI{
+    
+//    导航栏颜色
+    self.navigationBar.barTintColor = [UIColor hm_colorWithHex:0x3a3a3a];
+//    取消半透明效果
+    self.navigationBar.translucent = NO;
+    
+//    取消导航栏分隔线
+//    1、设置shadowImage图像为空
+    self.navigationBar.shadowImage = [[UIImage alloc]init];
+//    2、设置背景图像为空
+    [self.navigationBar setBackgroundImage:[[UIImage alloc]init]forBarMetrics:UIBarMetricsDefault];
+    
+//    设置导航栏字体颜色
+    self.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
+
 }
 
 /*
